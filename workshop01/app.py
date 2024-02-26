557224cf5d17bfc4ca2759649aa21cecd8e7ec27
Ian <ianho@dsaid.gov.sg>
2024-02-26 22:44:11 +0800

    changes

--- workshop01/app.py
+++ workshop01/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import random
+import os
 
 app = Flask(__name__)
 
@@ -17,4 +18,5 @@ def index():
     return render_template('index.html', quote=random_quote)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = os.environ.get("PORT", 5000)
+    app.run(host='0.0.0.0', port=port, debug=True)
